21bcb5002ce87b0f8ccd2f4315b960ab9bf7355a
gunasekhar-dev <80669471+gunasekhar-dev@users.noreply.github.com>
2022-05-21 23:51:45 +0530

    Add on

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { MenuComponent } from './pages/menu/menu.component';
 const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'menu', component:MenuComponent},
-  {path: 'abut', component:AboutComponent},
+  {path: 'about', component:AboutComponent},
   {path: 'contact', component:ContactComponent},
 
 ];
